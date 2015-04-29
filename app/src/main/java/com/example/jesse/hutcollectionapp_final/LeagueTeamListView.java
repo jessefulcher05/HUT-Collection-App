5b786bbfbea9894471ea8ee4650eb302ee611f14
Jesse Fulcher <jessefulcher05@gmail.com>
2015-04-29 16:32:21 -0600

    Changes to LeagueTeamListView

--- app/src/main/java/com/example/jesse/hutcollectionapp_final/LeagueTeamListView.java
+++ app/src/main/java/com/example/jesse/hutcollectionapp_final/LeagueTeamListView.java
@@ -8,9 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
